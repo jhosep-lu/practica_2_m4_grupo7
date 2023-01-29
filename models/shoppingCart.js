01615285582919c3bc3617af7b20fae9176d1ea1
jhosep-lu <jhosep.vi.lu@gmail.com>
2023-01-29 16:30:39 -0400

    modified model shoppingCart

---
+++ models/shoppingCart.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const shoppingCartSchema = new mongoose.Schema({
+    invoiceNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["PENDING", "PAID"],
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    user: {
+        type: String,
+        required: true,
+    },
+    products: [
+        {
+            productId: {
+                type: String,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+            price: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+
+});
